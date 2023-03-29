07ea2e5864a6ea4413b23bdb57013abf04ed0f52
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-03-30 00:08:36 +0200

    update

--- train/src/App.js
+++ train/src/App.js
@@ -1,12 +1,33 @@
-
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [items] = useState({
+    items1: 'pizza',
+    items2: 'ham',
+    items3: 'cookie',
+    items4: 'coca-cola',
+  })
+
+
+
+
   return (
     <div className="App">
-      Działa
+      <h1>Lista zakupów</h1>
+      <ul>
+        <ListItem name={items.items1} example={2 + 2} />
+        <ListItem name={items.items2}/>
+        <ListItem name={items.items3}/>
+        <ListItem name={items.items4}/>
+      </ul>
     </div>
   );
 }
+
+const ListItem = ({ name, example }) => (
+  < li > {name} : {example}</li>
+);
 
 export default App;
