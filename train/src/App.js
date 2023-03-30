ad7666f8a9187ac1a7a230adac05e795a512763e
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-03-30 23:31:59 +0200

    Update App.js

--- train/src/App.js
+++ train/src/App.js
@@ -1,30 +1,125 @@
 import { useState } from 'react';
 import './App.css';
 
+const SUBSTRACTION = 'substraction';
+const RESET = 'reset';
+const ADDITION = 'addition';
 
-const App = () => {
+const App = (props) => {
 
-  const [value, setValue] = useState('');
+  const [count, setCount] = useState(0);
+  const [result, setResult] = useState(0);
 
-  const handleOnChange = event => {
-   setValue(event.target.value);
-  }
-
-  const handleOnClick = () => {
-    setValue('');
+  const handleMathClick = (type, number = 1) => {
+    if (type === SUBSTRACTION) {
+      setCount(count + 1);
+      setResult(result - number);
+    } else if (type === RESET) {
+      setCount(count + 1);
+      setResult('0');
+    } else if (type === ADDITION) {
+      setCount(count + 1);
+      setResult(result + number);
+    }
   }
 
 
   return ( 
     <>
-      <input type="text" value={value} onChange={handleOnChange} />
-      <button onClick={handleOnClick}>Reset</button>
-      <h1>{value}</h1>
+      <MathButton
+        name='-10'
+        number={10}
+        type={SUBSTRACTION}
+        click={handleMathClick}
+      />
+      <MathButton
+        name='-1'
+        number={1}
+        type={SUBSTRACTION}
+        click={handleMathClick}
+      />
+      <MathButton
+        name='Reset'
+        type={RESET}
+        click={handleMathClick}
+      />
+      <MathButton
+        name='+10'
+        number={10}
+        type={ADDITION}
+        click={handleMathClick}
+      />
+      <MathButton
+        name='+1'
+        number={1}
+        type={ADDITION}
+        click={handleMathClick}
+      />
+      <ResultPanel count={count} result={result} />
     </>
    );
 }
+
+const MathButton = ({ click, number, type, name }) => {
+  const handleOnClick = () => click(type, number) 
+  return (
+    <button onClick={handleOnClick}>{name}</button>
+  )
+}
+
+const ResultPanel = ({ count, result }) => {
+  const additionInformation = count > 10 ?
+    (<span>.Przeciążenie procesora</span>) : null
+  
+  return (
+    <>
+      <h2>Liczba kliknięć: {count}{additionInformation}</h2>
+      <h2>Wynik: {result}</h2>
+    </>
+  )
+}
  
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const App = () => {
+
+//   const [value, setValue] = useState('');
+
+//   const handleOnChange = event => {
+//    setValue(event.target.value);
+//   }
+
+//   const handleOnClick = () => {
+//     setValue('');
+//   }
+
+
+//   return ( 
+//     <>
+//       <input type="text" value={value} onChange={handleOnChange} />
+//       <button onClick={handleOnClick}>Reset</button>
+//       <h1>{value}</h1>
+//     </>
+//    );
+// }
+ 
+// export default App;
 
 
 
